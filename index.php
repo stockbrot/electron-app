356be3daf0ed3ce1fcbb7b9934c418aafebb8440
doctororbit <taniswebb@gmail.com>
2018-01-18 20:41:59 +0100

    test

--- index.php
+++ index.php
@@ -8,9 +8,6 @@
   <script type="text/javascript" src="assets/js/tab.js"></script>
   <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
   <link rel="stylesheet" type="text/css" href="assets/css/main.css">
-  <!-- <script type="text/javascript" src="assets/js/bootstrap.bundle.js"></script> -->
-  <!-- <link rel="stylesheet" type="text/css" href="assets/vendors/bootstrap4/bootstrap-grid.min.css"> -->
-  <!-- <link rel="stylesheet" type="text/css" href="assets/vendors/bootstrap4/bootstrap-grid.min.css"> -->
   <!-- App & fonts-->
   <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:300,300i,400,400i,700,700i&amp;amp;subset=latin-ext">
   <!--[if lt IE 9]-->
